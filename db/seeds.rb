9fd0482bf9b09f11bd90cd59ca857a2a75086e34
Claire Ross <cross2021@gmail.com>
2023-11-16 13:49:03 +0000

    finalized api

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "json"
+require "open-uri"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+user_serialized = URI.open(url).read
+user = JSON.parse(user_serialized)
