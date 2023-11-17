dc4cdf91ae7fa1618e80e5f17109f99fe415c84e
Claire Ross <cross2021@gmail.com>
2023-11-17 14:29:03 +0000

    Prepare rails app with external specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get '/list', to: 'lists#index'
+  # get '/new', to: 'lists#new'
+  # get '/create', to: 'lists#create'
+  # post 'lists', to: 'lists#create'
+  resources :lists, only: [:index, :new, :show, :create ] do
+    resources :bookmarks, only: [ :new, :create]
 
+  end
+
+  resources :bookmarks, only: [:destroy]
+
+
+
+  # do
+    # resources :bookmarks, only: []
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,31 @@
 require "json"
 require "open-uri"
 
-url = "http://tmdb.lewagon.com/movie/top_rated"
-user_serialized = URI.open(url).read
-user = JSON.parse(user_serialized)
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+response= URI.open(url).read
+data = JSON.parse(response)
+movies_array = data["results"]
+img_url = "http://image.tmdb.org/t/p/w500"
+
+movies_array.each do |movie|
+  new_movie = Movie.new(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: img_url + movie["poster_path"],
+    rating: movie["vote_average"]
+  )
+  new_movie.save
+end
+
+puts "movies done! now for the list"
+puts "creating 3 lists"
+List.create!(name: "Watchlist")
+List.create!(name: "Completed")
+List.create!(name: "Upcoming")
+
+puts "all done! roll camera"
+
+# user["results"].each do |movie|
+#   movie.create(title: movie["title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original/#{movie["poster_path"]}", rating: movie["vote_average"])
+#   p movie
+# end
